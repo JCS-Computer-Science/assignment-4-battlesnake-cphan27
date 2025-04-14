07ca1cbb7be75dfb3bc181e9a13f464f6ead8599
cphan27 Phan <chantelp6@educbe.ca>
2025-04-14 17:55:47 +0000

    moves up

--- moveLogic.js
+++ moveLogic.js
@@ -6,6 +6,7 @@ export default function move(gameState){
         right: true
     };
     
+    
     // We've included code to prevent your Battlesnake from moving backwards
     const myHead = gameState.you.body[0];
     const myNeck = gameState.you.body[1];
@@ -26,16 +27,50 @@ export default function move(gameState){
     // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
     // gameState.board contains an object representing the game board including its width and height
     // https://docs.battlesnake.com/api/objects/board
-    
+    // const boardWidth = gameState.board.width;
+    // const boardHeight = gameState.board.height;
+
+    if(myHead.x == 0){
+        moveSafety.left = false;
+    }
+    if (myHead.y == 0){
+        moveSafety.down = false;
+    }
+    if (myHead.x == gameState.board.width - 1){
+        moveSafety.right = false;
+    }
+    if (myHead.y == gameState.board.height - 1){
+        moveSafety.up = false;
+    }
+
+
     // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     // gameState.you contains an object representing your snake, including its coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
+    for (let i = 1; i < (myBody.length - 1); i++) {
+        
+        const segment = myBody[i];
+        if (segment.x == myHead.x && segment.y == myHead.y + 1) {
+            moveSafety.up = false;
+        }
+        if (segment.x == myHead.x && segment.y == myHead.y - 1) {
+            moveSafety.down = false;
+        }
+        if (segment.x == myHead.x - 1 && segment.y == myHead.y) {
+            moveSafety.left = false;  
+        }
+        if (segment.x == myHead.x + 1 && segment.y == myHead.y) {
+            moveSafety.right = false;   
+        }
+    }
+
     
     
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
     
+
     // Are there any safe moves left?
     
     //Object.keys(moveSafety) returns ["up", "down", "left", "right"]
